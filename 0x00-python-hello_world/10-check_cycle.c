29905cc1dffe8b9ae7a0f25cca033a53881ade97
MadoXZA <timklo15@gmail.com>
2023-06-05 08:18:09 -0700

    Task 10 and advanced task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *stadig = list;
+	listint_t *vinnig = list;
+
+	if (!list)
+		return (0);
+
+	while (stadig && vinnig && vinnig->next)
+	{
+		stadig = stadig->next;
+		vinnig = vinnig->next->next;
+		if (slow == vinnig)
+			return (1);
+	}
+
+	return (0);
+}
